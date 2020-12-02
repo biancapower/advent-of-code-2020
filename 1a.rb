93e906362738b43aa2037733dd2e7847c336bb13
biancapower <bianca@biancapower.com>
2020-12-02 22:43:37 +1100

    find two numbers that sum to 2020

--- 1a.rb
+++ 1a.rb
@@ -3,4 +3,12 @@ file = File.open("1a-input.txt")
 # turn file data into an array of data
 file_data = file.readlines.map(&:to_i)
 
-p file_data
+nums = []
+
+i = 0
+while nums.length < 2
+  nums << file_data[i] if file_data.include?(2020 - file_data[i])
+  i += 1
+end
+
+p nums[0] * nums[1]
