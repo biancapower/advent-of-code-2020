551d9701536b19a35bdc07959996e6bff4a5b36f
biancapower <bianca@biancapower.com>
2020-12-05 22:07:20 +1100

    refactor

--- 3b.rb
+++ 3b.rb
@@ -2,20 +2,25 @@ file = File.open("3-input.txt")
 @data = file.readlines.map(&:chomp)
 
 @counter = (0..@data[0].length-1).to_a
-@sum = 0
 
-def count_trees
+def count_trees(across,down)
   loopy = @counter.cycle
   loopy.next
+  
+  sum = 0
 
-  for line in 1..@data.length - 1  
-    loopy.next
-    loopy.next 
-    x = loopy.next
+  for line in down..@data.length - 1  
+    x = 0
+    across.times {x = loopy.next}
     
-    @sum +=1 if @data[line][x] == "#"
+    sum +=1 if @data[line][x] == "#"
   end
+
+  sum
 end
 
-count_trees
-p @sum
+p count_trees(1,1)
+p count_trees(3,1)
+p count_trees(5,1)
+p count_trees(7,1)
+p count_trees(1,2)
