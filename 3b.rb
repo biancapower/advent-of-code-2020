6869797158bc77c0c83026be079a9b05f6dbafde
biancapower <bianca@biancapower.com>
2020-12-05 22:25:03 +1100

    solve

--- 3b.rb
+++ 3b.rb
@@ -8,12 +8,20 @@ def count_trees(across,down)
   loopy.next
   
   sum = 0
+  # line = down
+  i = down
 
-  for line in down..@data.length - 1  
+  # for line in down..@data.length - 1  
+  while i < @data.length
     x = 0
+    
     across.times {x = loopy.next}
     
-    sum +=1 if @data[line][x] == "#"
+    sum +=1 if @data[i][x] == "#"
+    # line += (down - 1) if !(line >= @data.length) # FIXME: not working for down = 2
+    
+    i += down
+    # i+=1
   end
 
   sum
@@ -24,3 +32,5 @@ p count_trees(3,1)
 p count_trees(5,1)
 p count_trees(7,1)
 p count_trees(1,2)
+
+p count_trees(1,1) * count_trees(3,1) * count_trees(5,1) * count_trees(7,1) * count_trees(1,2)
