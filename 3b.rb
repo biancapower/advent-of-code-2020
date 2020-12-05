8fd24ee1942b3437edf3d2083786fa8e189192af
biancapower <bianca@biancapower.com>
2020-12-05 20:35:02 +1100

    attempt 1, doesn't work

--- 3b.rb
+++ 3b.rb
@@ -3,4 +3,16 @@ file = File.open("3-input.txt")
 # turn file data into an array of data
 data = file.readlines.map(&:chomp)
 
-pp data[-1][2]
+sum = 0
+x = 0
+
+for line in 0..data.length - 1
+  x += 3
+  if x > data[line].length
+    data[line] *= x # way too hacky, makes the lines extremely unnecessarily long
+  end
+  
+  sum +=1 if data[line][x] == "#"
+end
+
+p sum
