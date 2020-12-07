7e41dd90e1d47152bc39e57c42d8f7ddc6309849
biancapower <bianca@biancapower.com>
2020-12-07 20:55:35 +1100

    add 5a (broken)

--- 5a.rb
+++ 5a.rb
@@ -2,21 +2,23 @@ file = File.open("5-input.txt")
 data = file.readlines.map(&:chomp)
 
 # for line in 0..data.length - 1
-  row_num = (0..127).to_a
+  row_min = 0 
+  row_max = 127
 
   for i in 0..6
     # case data[line][i]
     case data[0][i]
     when "F"  # lower half
-      row_num = (row_num[0]..((row_num.length-1)/2)).to_a
-      p "F: #{i}, Row Num: #{row_num}"
+      row_max = row_max / 2
+      # p "F: #{i}, Row Max: #{row_max}"
     when "B"  # upper half 
-      row_num = ((row_num.length/2)..row_num[-1]).to_a
-      p "B: #{i}, Row Num: #{row_num}"
+      row_min = row_max / 2 
+      # p "B: #{i}, Row Min: #{row_min}"
     else
       p "error: #{data[line][i]}"
     end
+    p "#{data[0][i]}: #{i}, Row Max: #{row_max}, Row Min: #{row_min}"
   end
-  p row_num
+  # p row_num
 # end
 # pp data
