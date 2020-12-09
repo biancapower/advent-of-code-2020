ff450f128e2c6b3932fe2b2a87ff2cb8be978fa9
Bianca Power <bianca@biancapower.com>
2020-12-09 12:03:08 +1100

    update but buggy

--- 5a.rb
+++ 5a.rb
@@ -1,31 +1,30 @@
 file = File.open("5-input.txt")
 @data = file.readlines.map(&:chomp)
 
-@total = []
+@total = [0,0]
 
-def analyse(min, max, i_start, i_end, lower_letter, upper_letter)
+def analyse(min, max, i_start, i_end, lower_letter, upper_letter, line, place)
 
   for i in i_start..i_end
-    # case @data[line][i]
-    case @data[0][i]
+    case @data[line][i]
+    # case @data[0][i]
     when lower_letter  # lower half
       max = ((max + min) / 2.0).floor
     when upper_letter  # upper half 
       min = ((max + min) / 2.0).ceil
-    else
-      # p "error: #{@data[line][i]}"
     end
     p "#{i}: #{@data[0][i]}, Max: #{max}, Min: #{min}"
   end
-  @total << (min + max) / 2
+  
+  @total[place] = min if @total[place] < min
 
 end
 
-# for line in 0..@data.length - 1
+for line in 0...@data.length
 
-  analyse(0, 127, 0, 6, "F", "B") # rows
-  analyse(0, 7, 7, 9, "L", "R") # cols
+  analyse(0, 127, 0, 6, "F", "B", line, 0) # rows
+  analyse(0, 7, 7, 9, "L", "R", line, 1) # cols
 
   # p row_num
-# end
+end
 pp @total[0]*8 + @total[1]
