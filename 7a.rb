2d7ede9d180bf3718d0ab327a6afa8f1bc7bff28
Bianca Power <bianca@biancapower.com>
2020-12-09 21:41:46 +1100

    convert data to hash

--- 7a.rb
+++ 7a.rb
@@ -1,4 +1,10 @@
 data = File.open("7-input.txt").readlines.map(&:chomp).map!{|x| x.split(" contain ")}
 gold = "shiny gold"
 
-pp data
+bags_rules = {}
+
+for i in 0...data.length
+  bags_rules[data[i][0]] = data[i][1]  
+end
+# pp data
+pp bags_rules
