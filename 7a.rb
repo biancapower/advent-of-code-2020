fc6c6e03865e8dd533ddfec090a4883b2de6e26b
Bianca Power <bianca@biancapower.com>
2020-12-09 21:56:44 +1100

    clean data

--- 7a.rb
+++ 7a.rb
@@ -1,10 +1,14 @@
 data = File.open("7-input.txt").readlines.map(&:chomp).map!{|x| x.split(" contain ")}
 gold = "shiny gold"
 
-bags_rules = {}
+all_rules = {}
 
 for i in 0...data.length
-  bags_rules[data[i][0]] = data[i][1]  
+  all_rules[data[i][0]] = data[i][1]  
 end
-# pp data
-pp bags_rules
+
+bag_rules = all_rules.transform_values do |value| 
+  value.split(", ") 
+end  
+
+pp bag_rules
