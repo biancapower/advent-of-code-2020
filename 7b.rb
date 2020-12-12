e4cd05424bb379dc48d05152c8c7c1a0531dd205
Bianca Power <bianca@biancapower.com>
2020-12-13 07:31:17 +1100

    fix variable naming

--- 7b.rb
+++ 7b.rb
@@ -1,20 +1,22 @@
 data = File.open("7-input.txt").readlines.map(&:chomp).map!{|x| x.split(" contain ")}
 gold = "shiny gold"
 
-bag_rules = {}
+@bag_rules = {}
 
 ########## PROCESS DATA ##########
 for i in 0...data.length
-  bag_rules[data[i][0]] = data[i][1]  
+  @bag_rules[data[i][0]] = data[i][1]  
 end
 
-bag_rules = bag_rules.transform_values do |value| 
+@bag_rules = @bag_rules.transform_keys do |key|
+  key = key[0...-5]
+end
+
+@bag_rules = @bag_rules.transform_values do |value| 
   value.split(", ").map!{|x| x.gsub(/(bag)s?\.?/, "")}.map!{|y| y.chop}
 end
 
-@bag_rules = bag_rules.transform_keys do |key|
-  key = key[0...-5]
-end
+
 ########## END PROCESS DATA ##########
 
 @container_colours = [gold]
@@ -32,4 +34,4 @@ for c in @container_colours
 end
     
 # pp @container_colours.uniq.count - 1
-pp bag_rules
+pp @bag_rules
