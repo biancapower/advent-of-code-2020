9eb006c8ad2b08ddb8897cd412cbf63d6845f32a
Bianca Power <bianca@biancapower.com>
2020-12-22 15:47:56 +1100

    refactor

--- 8b.rb
+++ 8b.rb
@@ -1,29 +1,54 @@
-data = File.open("8-input.txt").readlines.map(&:chomp).map!{|x| x.split(" ")}
+@data = File.open("8-input.txt").readlines.map(&:chomp).map!{|x| x.split(" ")}
 
 # convert numbers to ints
-for i in data
+for i in @data
   i[1] = i[1].to_i
 end
 
-# What leads to an infinite loop?
-accumulator = 0
-visited = []
+# these are in the initial infinite loop
+# one of these needs to change
 nops = []
 jmps = []
+
+# What leads to an infinite loop?
+# accumulator = 0
+visited = []
 
 i = 0
 while !visited.include?(i)
   visited << i
-  case data[i][0]
+  case @data[i][0]
   when "nop"
     nops << i
     i += 1
   when "acc"
-    accumulator += data[i][1]
+    # accumulator += @data[i][1]
     i += 1
   when "jmp"
     jmps << i
-    i += data[i][1]
+    i += @data[i][1]
   end
 end
-p accumulator
+
+@accumulator = 0
+
+def is_it_looping?
+  is_unique = []
+
+  j = 0
+  while !is_unique.include?(j)
+    is_unique << j
+    case @data[j][0]
+    when "nop"
+      j += 1
+    when "acc"
+      @accumulator += @data[j][1]
+      j += 1
+    when "jmp"
+      j += @data[j][1]
+    end
+  end
+  return false
+end
+is_it_looping?
+p @accumulator
