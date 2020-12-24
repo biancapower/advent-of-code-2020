49e6eecedf3ef62555d4751ba88434a7e14e5445
Bianca Power <bianca@biancapower.com>
2020-12-24 22:11:54 +1100

    solve 9b

--- 9b.rb
+++ 9b.rb
@@ -16,9 +16,39 @@ def invalid_sum
   end
 end
 
-p invalid_sum
 
 # find a range of sequential numbers within @data that sum to invalid_sum
+def subarray_sum(arr, sum)
+  current_sum = arr[0]
+  start = 0
+
+  i = 1
+  while i <= arr.length 
+    while current_sum > sum
+      p "#{current_sum} is too large"
+      current_sum = current_sum - arr[start] 
+      start += 1
+      p "now current_sum is #{current_sum}, start is now #{start}"
+    end
+    
+    if current_sum == sum
+      p "Sum found between indexes #{start} and #{i-1}"
+      return true
+    elsif current_sum < sum#i < arr.length 
+      p "#{current_sum} isn't there yet"
+      current_sum = current_sum + arr[i]
+    else
+      p "No valid subarray"
+      return false
+    end
+
+    i += 1
+  end
+end
+
+subarray_sum(@data, invalid_sum)
+# p @data[0]
+p "invalid: #{invalid_sum}"
 # add together the smallest and largest
   # arr.sort!
   # arr.min + arr.max
